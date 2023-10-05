cc023ba1d28fa53caa7ec2999fbfdcaca37a6b13
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-10-04 23:31:42 -0500

    feat: :construction: LOGIN ENCRYPTED

--- frontend/ecommerce/src/App.jsx
+++ frontend/ecommerce/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { HombrePage } from "./Pages/HombrePage";
 import { MujerPage } from "./Pages/MujerPage";
@@ -9,23 +9,62 @@ import { StorePage } from "./Pages/StorePage";
 
 import "./styles/App.css";
 import { User } from "./Pages/User";
-import {SignUp} from "./components/SignUp";
-import {SignIn} from "./components/SignIn";
+import { SignUp } from "./components/SignUp";
+import { SignIn } from "./components/SignIn";
+
+const isAuthenticated = () => {
+  return false;
+};
+
+const ProtectedRoute = ({ element, ...rest }) => {
+  return isAuthenticated() ? (
+    <Route {...rest} element={element}></Route>
+  ) : (
+    <Navigate to="/sign-in" />
+  );
+};
 
 export const App = () => {
+  const user = {
+    name: "Juan",
+    lastName: "Perez",
+    email: "juan@gmail.com",
+  };
+  {
+    /* <Route path="/" element={<MainContent />} />
+      <Route path="/men" element={<HombrePage />} />
+      <Route path="/woman" element={<MujerPage />} />
+      <Route path="/child-boy" element={<NinoPage />} />
+      <Route path="/child-girl" element={<NinaPage />} />
+      <Route path="/store-pay" element={<StorePage />} />
+      <Route path="/user-information" element={<User />} /> */
+  }
+
   return (
     <>
-      <NavBar />
       <Routes>
+        {!isAuthenticated() && null}
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/" element={<MainContent />} />
-        <Route path="/men" element={<HombrePage />} />
-        <Route path="/woman" element={<MujerPage />} />
-        <Route path="/child-boy" element={<NinoPage />} />
-        <Route path="/child-girl" element={<NinaPage />} />
-        <Route path="/store-pay" element={<StorePage />} />
-        <Route path="/user-information" element={<User />} />
+        <Route
+          path="/*"
+          element={
+            isAuthenticated() ? (
+              <>
+                <NavBar />
+                <MainContent />
+                <Route path="/men" element={<HombrePage />} />
+                <Route path="/woman" element={<MujerPage />} />
+                <Route path="/child-boy" element={<NinoPage />} />
+                <Route path="/child-girl" element={<NinaPage />} />
+                <Route path="/store-pay" element={<StorePage />} />
+                <Route path="/user-information" element={<User />} />
+              </>
+            ) : (
+              <Navigate to="/sign-in" />
+            )
+          }
+        />
       </Routes>
     </>
   );
